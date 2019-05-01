b916c9a2539865dd95d502997dc76000c427aea3
kennethj-CDH <kennyaj123@gmail.com>
2019-05-01 18:30:58 -0400

    got heatmap working for usaid

--- map.js
+++ map.js
@@ -1,72 +1,235 @@
 
+
 var colors = d3.scale.category10();
+// var currentMap = "#zoom_map";
+var div01 = document.querySelector("#zoom_map");
+var div02 = document.querySelector("#zoom_map02");
+heatmap_colors = ["f5f5f5", "red"];
 
-f = chroma.scale(["white", "darkred"]);
-
-// d3.scale.threshold()
-//     .range(['#8b0000','#a53c28','#bc634f','#d18978','#e3afa3','#f3d7d0','#ffffff']);
-    
-// function palette(min, max) {
-//     var d = (max-min)/7;
-//     return d3.scale.threshold()
-//         .range(['#8b0000','#a53c28','#bc634f','#d18978','#e3afa3','#f3d7d0','#ffffff'])
-//         .domain([min+1*d,min+2*d,min+3*d,min+4*d,min+5*d,min+6*d,min+7*d]);
-// }
+f = chroma.scale(["blue", "darkred"]).domain([0,5000000]);
 
 
+series =  [
+['ZAF', 1000 ],
+['ZWE', 2000 ],
+['NGA', 3000 ],
+['MOZ', 4000 ],
+['MDG', 5000 ],
+['EGY', 6000 ],
+['TZA', 7000 ],
+['LBY', 8000 ],
+['DZA',  9000],
+['SSD', 10000],
+['SOM', 11000],
+['GIB', 12000],
+['AGO', 13000]];
 
-var zoom = new Datamap({
-    element: document.getElementById("zoom_map"),
-    scope: 'world',
-    // Zoom in on Africa
-    
-    setProjection: function(element) {
-      var projection = d3.geo.equirectangular()
-        .center([23, -3])
-        .rotate([4.4, 0])
-        .scale(400)
-        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-      var path = d3.geo.path()
-        .projection(projection);
+series02 =  [
+    ['ZAF', 10000 ],
+    ['ZWE', 10000 ],
+    ['NGA', 3000 ],
+    ['MOZ', 4000 ],
+    ['MDG', 5000 ],
+    ['EGY', 6000 ],
+    ['TZA', 7000 ],
+    ['LBY', 8000 ],
+    ['DZA',  9000],
+    ['SSD', 10000],
+    ['SOM', 11000],
+    ['GIB', 12000],
+    ['AGO', 13000]];
+
+// var dataset = {};
+
+var onlyValues = series.map(function(obj){ return obj[1]; });
+var minValue = Math.min.apply(null, onlyValues);
+var maxValue = Math.max.apply(null, onlyValues);
+
+// fill dataset in appropriate format
+function fillDataset(seriesArr, colorFunc) {
+    funcDataset = {}
+    seriesArr.forEach(function(item){ //
+        // item example value ["USA", 70]
+        var iso = item[0],
+                value = item[1];
+                //console.log(value, colorFunc(value));
+                
+        funcDataset[iso] = { thedata: value, fillColor: colorFunc(value) };
+    });
+    //console.log(funcDataset);
+    return funcDataset;
+}
+
   
-      return {path: path, projection: projection};
-    },
-    fills: {
-      defaultFill: "#ABDDA4",
-      gt50: colors(Math.random() * 20),
-      eq50: colors(Math.random() * 20),
-      lt25: colors(Math.random() * 10),
-      gt75: colors(Math.random() * 200),
-      lt50: colors(Math.random() * 20),
-      eq0: colors(Math.random() * 1),
-      pink: '#0fa0fa',
-      gt500: colors(Math.random() * 1)
-    },
-    data: {
-      'ZAF': { fillKey: 'gt50', thedata: 1000 },
-      'ZWE': { fillKey: 'lt25', thedata: 2000},
-      'NGA': { fillKey: 'lt50', thedata: 3000 },
-      'MOZ': { fillKey: 'eq50', thedata: 4000 },
-      'MDG': { fillKey: 'eq50', thedata: 5000 },
-      'EGY': { fillKey: 'gt75', thedata: 6000 },
-      'TZA': { fillKey: 'gt75', thedata: 7000 },
-      'LBY': { fillKey: 'eq0', thedata: 8000 },
-      'DZA': { fillKey: 'gt500', thedata: 9000 },
-      'SSD': { fillKey: 'pink', thedata: 10000 },
-      'SOM': { fillKey: 'gt50', thedata: 11000 },
-      'GIB': { fillKey: 'eq50', thedata: 12000 },
-      'AGO': { fillKey: 'lt50', thedata: 13000 }
-    },
-    geographyConfig: {
-        popupTemplate: function(geo, data) {
-            return ['<div class="hoverinfo"><strong>',
-                    'Number of things in ' + geo.properties.name,
-                    ': ' + data.thedata,
-                    '</strong></div>'].join('');
+  function updateMap(func_series, color_param_func) {
+    outobj = {};
+    for (var i=0; i < func_series.length; i++)
+    {   
+        hexstr = chroma(color_param_func(func_series[i][1])).hex();
+        outobj[func_series[i][0]] = hexstr;
+    }
+    //console.log(outobj);
+    zoom.updateChoropleth(outobj);
+}
+
+let usaid_url = 'https://storage.googleapis.com/africa-data/usaid-disbursements.json';
+
+async function fetchJSON(urlparam) {
+    const response = await fetch(urlparam);
+    const json = await response.json();
+    return json;
+}
+
+var zoom;
+
+var queryYear = 2010;
+var dataSeries = [];
+var colorF = function(){};
+
+var usaid = {};
+var usaidPromise = fetchJSON(usaid_url);
+usaidPromise.then(
+    function (result) {
+        usaid = result;
+        // TODO: initialize map data values here because reasons
+        for (var i=0; i<usaid.length; i++) {
+            if (usaid[i].fiscal_year == queryYear) {
+                element = [usaid[i].country_code, usaid[i].current_amount];
+                dataSeries.push(element);
+            }
+        }  // end for
+        
+        var minValue = 0;
+        var maxValue = getMax(dataSeries);
+        console.log(maxValue);
+        colorF = chroma.scale(heatmap_colors).domain([0, maxValue]);
+        dataset = fillDataset(dataSeries, colorF);
+        //console.log(dataset);
+        
+
+        zoom = new Datamap({
+                element: document.getElementById("zoom_map"),
+                scope: 'world',
+                // Zoom in on Africa
+                
+                setProjection: function(element) {
+                  var projection = d3.geo.equirectangular()
+                    .center([23, -3])
+                    .rotate([4.4, 0])
+                    .scale(400)
+                    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+                  var path = d3.geo.path()
+                    .projection(projection);
+                    
+              
+                  return {path: path, projection: projection};
+                },
+                fills: {
+                  defaultFill: "slategray",
+                },
+                data: dataset,
+                
+                geographyConfig: {
+                    popupTemplate: function(geo, data) {
+                        return ['<div class="hoverinfo"><strong>',
+                                geo.properties.name,
+                                ': $' + formatNumber(data.thedata),
+                                '</strong></div>'].join('');
+                    }
+                 
+                }
+              });
+            
+    }
+);
+
+newMap = function (dataparam, year) {
+    
+    // if (currentMap === "#zoom_map") {
+    //     currentMap = "#zoom_map02";
+    // }
+    // else {
+    //     currentMap = "#zoom_map";
+    // }
+    dataSeries = [];
+    for (var i=0; i<dataparam.length; i++) {
+        if (dataparam[i].fiscal_year == year) {
+            element = [dataparam[i].country_code, dataparam[i].current_amount];
+            dataSeries.push(element);
+        }
+    }  // end for
+
+    var minValue = 0;
+    var maxValue = getMax(dataSeries);
+    console.log(maxValue);
+    colorF = chroma.scale(heatmap_colors).domain([0, maxValue]);
+    dataset02 = fillDataset(dataSeries, colorF);
+    //console.log(dataset);
+
+    var zoom02 = new Datamap({
+            //element: document.getElementById(currentMap.substr(1)),
+            element: document.getElementById("zoom_map"),
+            scope: 'world',
+            // Zoom in on Africa
+            
+            setProjection: function(element) {
+              var projection = d3.geo.equirectangular()
+                .center([23, -3])
+                .rotate([4.4, 0])
+                .scale(400)
+                .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+              var path = d3.geo.path()
+                .projection(projection);
+                console.log(path);
+          
+              return {path: path, projection: projection};
+            },
+            fills: {
+              defaultFill: "slategray",
+            },
+            data: dataset02,
+            
+            geographyConfig: {
+                popupTemplate: function(geo, data) {
+                    return ['<div class="hoverinfo"><strong>',
+                            geo.properties.name,
+                            ': ' + formatNumber(data.thedata),
+                            '</strong></div>'].join('');
+                }
+             
+            }
+    });
+ 
+    replaceMap();
+}
+
+function replaceMap() {
+    el = document.querySelector("#zoom_map");
+    console.log(el.childNodes);
+    el.removeChild(el.childNodes[4]);
+    el.removeChild(el.childNodes[3]);
+}
+
+function getMax(arr) {
+    max = -1; 
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i][1] > max) {
+            max = arr[i][1];
         }
     }
-  });
+    return max;
+}
+function getMin(arr) {
+    min = Number.MAX_SAFE_INTEGER; 
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i][1] < min) {
+            min = arr[i][1];
+        }
+    }
+    return min;
+}
 
-  function generateFillColor(value) {
-
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
   }
+//console.log(usaid);
